7ff39453b9c2f9a2137c7bbaa04336b97d3e631d
tonmoybot2 <fftonmoy67@gmail.com>
2024-12-12 19:48:54 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Tonmoy")==0 || (event.body.indexOf("তন্ময়")==0 || (event.body.indexOf("Tonmoy Choudhury")==0 || (event.body.indexOf("@Tonmoy Choudhury")==0)))) {
+	if (event.body.indexOf("Tonmoy")==0 || (event.body.indexOf("তন্ময়")==0 || (event.body.indexOf("@Tonmoy Choudhury")==0 || (event.body.indexOf("@Tonmoy Choudhury")==0)))) {
 		var msg = {
 				body: "আসসালামু আলাইকুম এই যে জান তুমি কি হচ্ছে আমার বস তন্ময় কে ডাকছো? সে তো এখন ব্যস্ত আছে যদি কিছু বলার থাকে বসের ইনবক্সে নক করে রাখো fb https://www.facebook.com/tonmoy221?mibextid=ZbWKwL 😇😻 :))"
     }
